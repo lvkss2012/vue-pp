1b932748ac9daafa06963298a87ff41006b19a30
刁世杰 <805386312@qq.com>
2019-06-05 14:43:07 +0800

    add

--- webpack.common.js
+++ webpack.common.js
@@ -15,6 +15,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.vue$/,
                 exclude: /^node_modules$/,
                 use: [
