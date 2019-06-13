06861556924d8973bb2b973deebeea08fe75430d
刁世杰 <805386312@qq.com>
2019-06-13 10:24:28 +0800

    添加markdown编辑器

--- src/components/page/ArticluePage.vue
+++ src/components/page/ArticluePage.vue
@@ -1,13 +1,58 @@
 <template>
-    <h3>文章列表</h3>
+    <div>
+        <el-row :gutter="10">
+            <el-col :span="1">
+                <el-tree
+                        class="filter-tree"
+                        :data="menu"
+                        :props="defaultProps"
+                        default-expand-all
+                        :filter-node-method="filterNode"
+                        ref="tree">
+                </el-tree>
+            </el-col>
+            <el-col :span="23">
+                <Markdown/>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
+    import Markdown from './Markdown.vue'
+
     export default {
-        name: "ArticluePage"
+        name: "ArticluePage",
+        components: {
+            Markdown
+        },
+        data: function () {
+            return {
+                menu: [
+                    {
+                        id: 'lsy',
+                        label: 'lsy'
+                    },
+                    {
+                        id: 'lhh',
+                        label: 'lhh'
+                    },
+                    {
+                        id: 'dsj',
+                        label: 'dsj'
+                    },
+                ],
+                defaultProps: {
+                    children: 'children',
+                    label: 'label'
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .filter-tree {
+        /*background-color: rgba(210, 28, 226, 0.35);*/
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router'
 import ElementUI from 'element-ui';
+import axios from 'axios';
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = true;
+Vue.prototype.$axios = axios;
 
 Vue.use(ElementUI);
 
