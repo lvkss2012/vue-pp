462ce8c3959986da9a10af392bf540e2312816e3
刁世杰 <805386312@qq.com>
2019-08-13 22:57:50 +0800

    add

--- src/components/common/Navigation.vue
+++ src/components/common/Navigation.vue
@@ -5,7 +5,7 @@
             mode="horizontal"
             router
     >
-        <el-menu-item index="hrManager" key="hrManager">管理</el-menu-item>
+        <el-menu-item index="hrManager" key="hrManager">招聘管理</el-menu-item>
         <el-menu-item index="links" key="links">导航</el-menu-item>
         <el-menu-item index="articles" key="articles">文章</el-menu-item>
         <el-menu-item index="donates" key="donates">打赏</el-menu-item>
